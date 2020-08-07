b9b975415f5120ed440bad0315a27c1edcc48ab3
Alex P <alexeek@protonmail.com>
2020-08-07 09:30:33 +0100

    feat: add workbench ws

--- src/publish.rs
+++ src/publish.rs
@@ -9,23 +9,25 @@ use console::style;
 use crate::models::request::{File, Project, Publish, Script};
 use crate::util::{Settings, Spinner};
 
+use futures_util::{pin_mut, StreamExt};
+use tokio_tungstenite::connect_async;
+
 const API_ENDPOINT: &str = "https://pylon.bot/api";
 
 #[derive(Deserialize)]
 struct PublishResponse {
     name: String,
     revision: i64,
-    // TODO: Implement workbench WS
     workbench_url: String,
 }
 
-pub async fn handle() -> Result<(), Box<dyn std::error::Error>> {
+pub async fn handle(no_ws: bool) -> Result<(), Box<dyn std::error::Error>> {
     let cfg = match Settings::new() {
         Ok(x) => x,
         Err(e) => return Err(e.into()),
     };
 
-    let total = 2;
+    let total = if no_ws { 2 } else { 3 };
     let mut current = 1;
 
     let sp = Spinner::new("Running build command", "Built bundle", current, total);
@@ -79,6 +81,7 @@ pub async fn handle() -> Result<(), Box<dyn std::error::Error>> {
                 sp.err(&parsed);
             } else {
                 sp.done();
+                current += 1;
 
                 let parsed = res.json::<PublishResponse>().await?;
                 println!(
@@ -87,6 +90,25 @@ pub async fn handle() -> Result<(), Box<dyn std::error::Error>> {
                         .green()
                         .bright()
                 );
+
+                if !no_ws {
+                    let sp = Spinner::new("Opening workbench", "Workbench opened", current, total);
+
+                    // This really isn't too efficient, but it's good enough for its purpose.
+                    let (ws_stream, _) =
+                        connect_async(url::Url::parse(&parsed.workbench_url)?).await?;
+                    sp.done();
+                    let (_, read) = ws_stream.split();
+
+                    let ws_to_stdout = {
+                        read.for_each(|message| async {
+                            println!("{}", message.unwrap().to_string());
+                        })
+                    };
+
+                    pin_mut!(ws_to_stdout);
+                    ws_to_stdout.await;
+                }
             }
         }
         Err(e) => sp.err(&format!("{}", e)),
